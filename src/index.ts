96879b7431e050b04a1512579ab410b4cad909f1
GLEADS0\thuantran <thuan.tran@gleads.vn>
2023-02-16 10:42:32 +0700

    commit

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ import express, { Request, Response } from "express";
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get("/", (_req: Request, res: Response) => {
+app.get("/api", (_req: Request, res: Response) => {
     return res.send("Express Typescript on Vercel");
 });
 
-app.get("/ping", (_req: Request, res: Response) => {
+app.get("/api/ping", (_req: Request, res: Response) => {
     return res.send("pong 🏓");
 });
 
