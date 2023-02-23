a1770623162958adf9f6d88405d962a92475d6d1
GLEADS0\thuantran <thuan.tran@gleads.vn>
2023-02-23 09:09:23 +0700

    update change password

--- src/routes/auth/control.ts
+++ src/routes/auth/control.ts
@@ -5,12 +5,14 @@ import {
     checkEmail,
     google,
     profile,
+    setPassword,
     signIn,
 } from "./handlers";
 
 export const control: Control = {
     "access-token": accessToken,
     "change-password": changePassword,
+    "set-password": setPassword,
     "check-email": checkEmail,
     profile: profile,
     "google/sign-in": google.signIn,
